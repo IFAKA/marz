205b8ce18a78a9ec2971b41853c237b9ffa3382a
IFAKA <me@faka.dev>
2022-11-08 13:27:29 -0300

    fixed photo container grid

--- components/libs/InfiniteScroll/InfiniteScroll.tsx
+++ components/libs/InfiniteScroll/InfiniteScroll.tsx
@@ -10,7 +10,7 @@ export const InfiniteScroll = ({ children }: IChild) => {
             dataLength={length}
             hasMore={hasMore}
             loader={<Spinner />}
-            className={"grid sm:grid-cols-1 md:grid-cols-2 gap-4 p-4"}
+            className={"grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl::grid-cols-4 gap-4 p-4"}
             next={next}
         >
             {children}
